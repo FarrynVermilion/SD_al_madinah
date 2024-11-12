28f39f9da0ea899af6a64913ff56aff946ea38a8
FarrynVermilion <150990057+FarrynVermilion@users.noreply.github.com>
2024-11-12 09:08:37 +0700

    fixed rute izin user

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,9 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Symfony\Component\HttpKernel\Profiler\Profile;
 
 Route::get('/', function () {
     return view('auth.login');
@@ -14,17 +16,17 @@ Auth::routes();
 //rute untuk semua role
 Route::group(['middleware' => 'auth'], function () {
 	Route::get(uri: '/home', action: [HomeController::class,'index'])->name('home');
-    Route::get('register', [UserController::class,'register'])->name('register');
-    Route::post('createUser', [UserController::class,'create'])->name('create');
-    Route::get('user.index', [UserController::class,'index'])->name('user.index');
-    Route::resource('user', UserController::class,['except'=>'show']);
-	Route::get('profile', ['as' => 'profile.edit', 'uses' => 'App\Http\Controllers\ProfileController@edit']);
-	Route::put('profile', ['as' => 'profile.update', 'uses' => 'App\Http\Controllers\ProfileController@update']);
-	Route::put('profile/password', ['as' => 'profile.password', 'uses' => 'App\Http\Controllers\ProfileController@password']);
+	Route::get('profile', [ProfileController::class,'edit'])->name('profile.edit');
+	Route::put('profile', [ProfileController::class,'update'])->name('profile.update');
+	Route::put('profile/password', [ProfileController::class,'password'])->name('profile.password');
 });
 
 // rute khusus admin
 Route::middleware(['auth','user-access:Admin'])->group(function(){
+    Route::get('register', [UserController::class,'register'])->name('register');
+    Route::post('createUser', [UserController::class,'create'])->name('create');
+    Route::get('user.index', [UserController::class,'index'])->name('user.index');
+    Route::resource('user', UserController::class);
     Route::get('{page}', ['as' => 'page.index', 'uses' => 'App\Http\Controllers\PageController@index']);
 });
 
