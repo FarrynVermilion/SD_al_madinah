7a59241a68bd650412bfa910fcdf0236ef06ff1b
A Reynal W <anandareynalw@gmail.com>
2025-05-31 16:52:36 +0700

    hash login penjelasan

--- routes/api.php
+++ routes/api.php
@@ -5,13 +5,22 @@ use Illuminate\Support\Facades\Route;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 
-
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', function (Request $request) {
+        Auth::user()->tokens()->delete();
+        return response()->json([
+            'message' => 'logout success'
+        ]);
+    });
+});
 Route::post('/token/request', function (Request $request) {
     $request->validate([
         'email' => 'required|email',
@@ -21,11 +30,28 @@ Route::post('/token/request', function (Request $request) {
 
     $user = User::where('email', $request->email)->first();
 
-    if (! $user || ! Hash::check($request->password, $user->password)) {
-        throw ValidationException::withMessages([
-            'email' => ['The provided credentials are incorrect.'],
-        ]);
+    if (!$user) {
+        return response(['payback' => 'Account not found']);
+    } else if (!Hash::check($request->password, $user->password)) {
+        return response(['payback' => 'Incorrect Credentials']);
     }
 
-    return $user->createToken($request->device_name)->plainTextToken;
+    if (count(DB::table('personal_access_tokens')->where('tokenable_id', $user->id)->get()) > 0) {
+        DB::table('personal_access_tokens')->where('tokenable_id', $user->id)->delete();
+    }
+
+    $tokenGen = $user->createToken('access_token')->plainTextToken;
+    $token = explode('|', $tokenGen)[1];
+    $hashed = DB::table('personal_access_tokens')->where('tokenable_id', $user->id)->get()[0]->token;
+    $correct = hash_equals($hashed, hash('sha256', $token));
+
+    $response = [
+        'user'  => $user,
+        'token' => $tokenGen,
+        'valid' => $correct,
+        'DB' => DB::table('personal_access_tokens')->where('tokenable_id', $user->id)->get()
+    ];
+
+    return $response;
 });
+
