31bf288d9e69380275556195f8d3be87cd442919
A Reynal W <anandareynalw@gmail.com>
2025-06-01 02:44:50 +0700

    buat api transaksi

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Transaksi_SPP;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\User;
@@ -59,4 +60,33 @@ Route::post('/token/request', function (Request $request) {
 
     return $response;
 });
+Route::post('/transaction', function (Request $request) {
+    $getToken = $request->token;
+    $id = explode('|', $getToken)[0];
+    $token = explode('|', $getToken)[1];
+    $record = DB::table('personal_access_tokens')->where('id', $id)->get()[0];
+    if(hash_equals($record->token, hash('sha256', $token))){
+        if (User::where('id', $record->tokenable_id)->get()[0]->role != 'Siswa') {
+            return response()->json([
+                'message' => 'unauthorized'
+            ]);
+        }
+        $transction = Transaksi_SPP::withTrashed()
+        ->join('spp_siswa', 'spp_siswa.id_spp_siswa', '=', 'transaksi_spp.id_spp')
+        ->join('database_biodata_siswa', 'database_biodata_siswa.id', '=', 'spp_siswa.id_siswa')
+        ->join('users', 'users.id', '=', 'database_biodata_siswa.id')
+        ->where('database_biodata_siswa.id_account',$record->tokenable_id)
+        ->select(
+            'transaksi_spp.*',
+            'database_biodata_siswa.nama_lengkap',
+            'database_biodata_siswa.id_account'
+        )->get();
 
+        return response()->json([
+            'message' => 'tansaction request success',
+            'data' => $transction,
+        ]);
+    }
+
+
+});
